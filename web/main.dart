962b7a2d15cb3086a764d44fc2a3bcba47c2d764
vapirovschi <vapi>
2024-12-17 10:47:33 +0200

    update

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,24 @@
-import 'package:web/web.dart' as web;
+import 'dart:html';
 
 void main() {
-  final now = DateTime.now();
-  final element = web.document.querySelector('#output') as web.HTMLDivElement;
-  element.text = 'The time is ${now.hour}:${now.minute} '
-      'and your Dart web app is running!';
+  // Get references to DOM elements
+  final inputField = querySelector('#item-input') as InputElement;
+  final addButton = querySelector('#add-button') as ButtonElement;
+  final clearButton = querySelector('#clear-button') as ButtonElement;
+  final itemList = querySelector('#item-list') as UListElement;
+
+  // Event listener to add items
+  addButton.onClick.listen((_) {
+    final itemText = inputField.value?.trim();
+    if (itemText != null && itemText.isNotEmpty) {
+      final listItem = LIElement()..text = itemText;
+      itemList.children.add(listItem);
+      inputField.value = ''; // Clear the input field
+    }
+  });
+
+  // Event listener to clear all items
+  clearButton.onClick.listen((_) {
+    itemList.children.clear();
+  });
 }
